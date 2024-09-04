f61a7fa451f9857a0242685524cdfab348484eee
Elizabeth Giordano <egiorda6@jh.edu>
2024-09-04 17:06:40 -0400

    grf2bed.py

---
+++ d2/day2-afternoon/gtf2bed.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+
+import sys
+
+my_file = open( sys.argv[1] )   
+
+for my_line in my_file:
+    if "##" in my_line:
+            continue
+    fields = my_line.split("\t")
+    print(fields[0], fields[3], fields[4], fields[8])
+
+my_file.close()
+
+# for each line in my data
+# separate by tabs/column (split)
+# select out columns I want
+# print
+    
