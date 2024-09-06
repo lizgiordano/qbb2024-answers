30b403e186e958862e33b03d16203a086b7b2958
Elizabeth Giordano <egiorda6@jh.edu>
2024-09-06 08:25:58 -0400

    grep.py.1

--- d2/day2-afternoon/grep.py
+++ d2/day2-afternoon/grep.py
@@ -5,10 +5,12 @@ import sys
 my_file = open( sys.argv[2] )   
 pattern = sys.argv[1]
 
-for my_line in my_file:
+for my_line in my_file:..
     my_line = my_line.rstrip("\n")
     if pattern in my_line:
         print(my_line)
-   
 
 my_file.close()
+   
+# in terminal: (qb24) cmdb@QuantBio-21 day2-afternoon % ./grep.py FIS1 ../day2-morning/gencode.v46.basic.annotation.gtf | less -S
+
