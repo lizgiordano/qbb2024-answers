1196ae01eae7caf7341871d6e5b0d23a3cb6119c
Elizabeth Giordano <egiorda6@jh.edu>
2024-09-11 14:34:07 -0400

    day 4 afternoon assignment

--- d4/Day4_afternoon_assignment.R
+++ d4/Day4_afternoon_assignment.R
@@ -28,7 +28,7 @@ print (dnm_by_parental_age)
 # 1 maternal de novo mutations vs maternal age
 ggplot(data=dnm_by_parental_age, mapping = aes(x=Mother_age, y=n_maternal_dnm))+
   geom_point()
-# 2 paternal de novo mutations vs paternal ge
+# 2 paternal de novo mutations vs paternal age
 ggplot(data=dnm_by_parental_age, mapping = aes(x=Father_age, y=n_paternal_dnm))+
   geom_point()
 # step 2.2
@@ -41,7 +41,10 @@ ggplot(data=dnm_by_parental_age,
                    y=n_maternal_dnm))+
   geom_point()+
   stat_smooth(method = "lm")
+# discuss the size and significance of the relationship. 
+# The average child has 2.5 DNMs. Approximately 1 mutation is gained for every 3 years the mother ages.
 # increase in mother age increases the chance of maternal de novo mutations. This does match with plots in step 2.1
+# This is significant and of high size because the p-value is so small (exponent of -16), suggesting there is a relationship between maternal age and mutations.
 
 # 2 
 # the relationship is significant because the p-value is so small. So this means that the observed relationship is not due to chance.
@@ -55,23 +58,30 @@ ggplot(data=dnm_by_parental_age,
                    y=n_paternal_dnm))+
   geom_point()+
   stat_smooth(method = "lm")
-# 1 As paternal age increases, the chance of paternal de novo mutations also increases
-# 2 the relationship is significant because the p-value is so small. So this means that the observed relationship is not due to chance.
+# discuss the size and significance of the relationship
+# 1 As paternal age increases, the chance of paternal de novo mutations also increases. The average child has 10 dnm, and gains 1 mutation with every year of paternal age.
+# 2 the relationship is significant with high size because the p-value is so small (exponent of -16). So this means that the observed relationship, that mutations increase with paternal age increase, is not due to chance.
 
 # Step 2.4
-
-
+father_a <- tibble(
+  paternal_age = 50.5
+)
+print(predict(paternal_model,father_a))
 
 
 # Step 2.5
 ggplot(data=dnm_by_parental_age) + 
   geom_histogram(mapping = aes(x = n_paternal_dnm, alpha = 0.5)) +
+  ylab("Frequency")+
+  xlab("Number of Paternal de Novo Mutations")
+
+ggplot(data=dnm_by_parental_age) + 
   geom_histogram(mapping = aes(x = n_maternal_dnm, alpha = 0.5)) +
   ylab("Frequency")+
-  xlab("Number of de Novo Mutations")
+  xlab("Number of Maternal de Novo Mutations")
 
 
 # Step 2.6
 t.test(dnm_by_parental_age$n_paternal_dnm, dnm_by_parental_age$n_maternal_dnm, paired = TRUE)
-# chose a paired t-test because I am trying to find the significant difference between two measurements. In this case, the difference between maternal and paternal mutations.
+# I chose a paired t-test because I am trying to find the significant difference between two measurements. In this case, the difference between maternal and paternal mutations.
 # My test was statistically significant. This means that there is a difference between maternal and faternal mutation rates.
