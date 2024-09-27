c305a1d940a1568915b40911f64dd20e5d6bc5f4
Elizabeth Giordano <egiorda6@jh.edu>
2024-09-27 11:17:27 -0400

    step 2 R script

--- qb24/week2/week2_step2.R
+++ qb24/week2/week2_step2.R
@@ -4,18 +4,15 @@ library(ggplot2)
 # add in data
 snp_data <- read.delim("~/qbb2024-answers/qb24/week2/snp_counts.txt") # pull the snp counts file and read through it
 # log2 transform enrichment on the Y-axis
-snp_data$log_enrichment <- log2(snp_data$enrichment)
+snp_data$enrichment <- as.numeric(snp_data$enrichment)
+snp_data <- snp_data %>% mutate(log_enrichment = log2(enrichment)) 
 
 # plot with ggplot
-ggplot <- ggplot() +
-  geom_line(data=snp_data, aes(x = MAF, y = log_enrichment, color = feature, group = Feature)) +
-  # graphs data
-  scale_color_colorblind() + # colorblind setting
-  labs(title = "SNP Enrichment by MAF and Feature", # for legend
+ggplot() +
+  geom_line(data = snp_data, aes(x = MAF, y = log_enrichment, color = Feature, group = Feature)) +
+  geom_line(linewidth = 1) +  # Add lines for each Feature
+  labs(title = "SNP Enrichment by MAF and Feature",  # for legend
        x = "Minor Allele Frequency (MAF)",
        y = "Log2(Enrichment)",
-       color = "Feature") + 
-  ggsave(filename=~/qbb2024-answers/qb24/week2/enrichments_plot.pdf)
-
-enrichments_plot.pdf
-
+       color = "Feature")
+ggsave(filename = path.expand("~/qbb2024-answers/qb24/week2/enrichments_plot.pdf"))
