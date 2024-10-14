2fca80783dbcd133d19d463700807988bfa168da
Elizabeth Giordano <egiorda6@jh.edu>
2024-10-14 14:44:25 -0400

    R script

--- qb24/week5/week5.R
+++ qb24/week5/week5.R
@@ -34,75 +34,80 @@ data = data %>% mutate_if(is.numeric, as.integer)
 # Remove low coverage samples
 data = data[rowSums(data) > 100,]
 
-# Pull out broad region samples
-broad = data %>% select("A1-3_Rep1":"P1-4_Rep3")
+# Pull out narrow-midgut-tissues
+narrow = data %>% select("A1_Rep1":"P2-4_Rep3")
 
 # Create metadata tibble with tissues and replicate numbers based on sample names
-broad_metadata = tibble(tissue=as.factor(c("A1_3", "A1_3", "A1_3", "Cu_LFC_Fe", "Cu_LFC_Fe", "Cu_LFC_Fe", "P1_4", "P1_4", "P1_4")),
-                        rep=as.factor(c(1, 2, 3, 1, 2, 3, 1, 2, 3)))
+narrow_metadata = tibble(Tissue=as.factor(c("A1", "A1", "A1", "A2-3", "A2-3", "A2-3", "Cu", "Cu", "Cu", "LFC-Fe", "LFC-Fe", "Fe", "LFC-Fe", "Fe", "Fe", "P1", "P1", "P1", "P2-4","P2-4","P2-4")),
+                         Replicate=as.factor(c("Replicate1", "Replicate2", "Replicate3", "Replicate1", "Replicate2", "Replicate3", "Replicate1", "Replicate2", "Replicate3",
+                                               "Replicate1", "Replicate2", "Replicate1", "Replicate3", "Replicate2", "Replicate3", "Replicate1", "Replicate2", "Replicate3", "Replicate1", "Replicate2", "Replicate3")))
+
 
 ### Step 3.2 ####
 # Create a DESeq data object
-broaddata = DESeqDataSetFromMatrix(countData=as.matrix(broad), colData=broad_metadata, design=~tissue)
+narrowdata = DESeqDataSetFromMatrix(countData=as.matrix(narrow), colData=narrow_metadata, design=~Tissue)
 
+narrowvstdata=vst(narrowdata)
+                  
 # Plot variance by average
-meanSdPlot(assay(broaddata))
+meanSdPlot(assay(narrowvstdata))
 
-# Log transform data
-broadLogdata = normTransform(broaddata) # log(1 + data)
+narrowvstPCAdata = plotPCA(narrowvstdata,intgroup=c("Replicate","Tissue"), returnData=TRUE)
+ggplot(narrowvstPCAdata, aes(PC1, PC2, color=Tissue, shape=Replicate)) +
+  geom_point(size=5) +
+  labs(title = "PCA - Midgut Tissues")
+  ggsave("~/qbb2024-answers/week5/PCAplot.png")
 
-# Plot log-transformed data variance by average
-meanSdPlot(assay(broadLogdata))
 
 
 ### step 3.3 ###
 
 # Create PCA data
-broadPcaData = plotPCA(broadLogdata,intgroup=c("rep","tissue"), returnData=TRUE)
+narrowPcaData = plotPCA(narrowLogdata,intgroup=c("rep","tissue"), returnData=TRUE)
 
 # Plot PCA data
-ggplot(broadPcaData, aes(PC1, PC2, color=tissue, shape=rep)) +
+ggplot(narrowPcaData, aes(PC1, PC2, color=tissue, shape=rep)) +
   geom_point(size=5)
 
 # Batch-correct data to remove excess variance with variance stabilizing transformation
-broadVstdata = vst(broaddata)
+narrowVstdata = vst(narrowdata)
 
 # Plot variance by average to verify the removal of batch-effects
-meanSdPlot(assay(broadVstdata))
+meanSdPlot(assay(narrowVstdata))
 
 # Perform PCA and plot to check batch-correction
-broadPcaData = plotPCA(broadVstdata,intgroup=c("rep","tissue"), returnData=TRUE)
-ggplot(broadPcaData, aes(PC1, PC2, color=tissue, shape=rep)) +
+narrowPcaData = plotPCA(narrowVstdata,intgroup=c("rep","tissue"), returnData=TRUE)
+ggplot(narrowPcaData, aes(PC1, PC2, color=tissue, shape=rep)) +
   geom_point(size=5)
 
 # Convert into a matrix
-broadVstdata = as.matrix(assay(broadVstdata))
+narrowVstdata = as.matrix(assay(narrowVstdata))
 
 # Find replicate means
-combined = broadVstdata[,seq(1, 9, 3)]
-combined = combined + broadVstdata[,seq(2, 9, 3)]
-combined = combined + broadVstdata[,seq(3, 9, 3)]
+combined = narrowVstdata[,seq(1, 21, 3)]
+combined = combined + narrowVstdata[,seq(2, 21, 3)]
+combined = combined + narrowVstdata[,seq(3, 21, 3)]
 combined = combined / 3
 
 # Use replicate means to filter low variance genes out
 filt = rowSds(combined) > 1
-broadVstdata = broadVstdata[filt,]
+narrowVstdata = narrowVstdata[filt,]
 
 # Plot expression values with hierarchical clustering
-heatmap(broadVstdata, Colv=NA)
+heatmap(narrowVstdata, Colv=NA)
 
 # Perform new hierarchical clustering with different clustering method
-distance = dist(broadVstdata)
+distance = dist(narrowVstdata)
 Z = hclust(distance, method='ave')
 
 # Plot expression values with new hierarchical clustering
-heatmap(broadVstdata, Colv=NA, Rowv=as.dendrogram(Z))
+heatmap(narrowVstdata, Colv=NA, Rowv=as.dendrogram(Z))
 
 # Set seed so this clustering is reproducible
 set.seed(42)
 
 # Cluster genes using k-means clustering
-k=kmeans(broadVstdata, centers=12)$cluster
+k=kmeans(narrowVstdata, centers=12)$cluster
 
 # Find ordering of samples to put them into their clusters
 ordering = order(k)
@@ -111,15 +116,15 @@ ordering = order(k)
 k = k[ordering]
 
 # Plot heatmap of expressions and clusters
-heatmap(broadVstdata[ordering,],Rowv=NA,Colv=NA,RowSideColors = RColorBrewer::brewer.pal(12,"Paired")[k])
+heatmap(narrowVstdata[ordering,],Rowv=NA,Colv=NA,RowSideColors = RColorBrewer::brewer.pal(12,"Paired")[k])
 
 # Save heatmap
 png("heatmap.jpg")
-heatmap(broadVstdata[ordering,],Rowv=NA,Colv=NA,RowSideColors = RColorBrewer::brewer.pal(12,"Paired")[k])
+heatmap(narrowVstdata[ordering,],Rowv=NA,Colv=NA,RowSideColors = RColorBrewer::brewer.pal(12,"Paired")[k])
 dev.off()
 
 # Pull out gene names from a specific cluster
-genes = rownames(broadVstdata[k == 9,])
+genes = rownames(narrowVstdata[k == 9,])
 
 # Same gene names to a text file
 write.table(genes, "cluster_genes.txt", sep="\n", quote=FALSE, row.names=FALSE, col.names=FALSE)
