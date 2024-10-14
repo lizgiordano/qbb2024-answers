34d60ef6d2cffd4895b9e453e77c477c478e263c
Elizabeth Giordano <egiorda6@jh.edu>
2024-10-14 15:28:09 -0400

    R script

--- qb24/week5/week5.R
+++ qb24/week5/week5.R
@@ -38,76 +38,72 @@ data = data[rowSums(data) > 100,]
 narrow = data %>% select("A1_Rep1":"P2-4_Rep3")
 
 # Create metadata tibble with tissues and replicate numbers based on sample names
+# helps you see how replicates cluster by tissue type.
 narrow_metadata = tibble(Tissue=as.factor(c("A1", "A1", "A1", "A2-3", "A2-3", "A2-3", "Cu", "Cu", "Cu", "LFC-Fe", "LFC-Fe", "Fe", "LFC-Fe", "Fe", "Fe", "P1", "P1", "P1", "P2-4","P2-4","P2-4")),
+                         # identify replicate samples for each tissue
                          Replicate=as.factor(c("Replicate1", "Replicate2", "Replicate3", "Replicate1", "Replicate2", "Replicate3", "Replicate1", "Replicate2", "Replicate3",
                                                "Replicate1", "Replicate2", "Replicate1", "Replicate3", "Replicate2", "Replicate3", "Replicate1", "Replicate2", "Replicate3", "Replicate1", "Replicate2", "Replicate3")))
 
 
 ### Step 3.2 ####
+
 # Create a DESeq data object
+# The DESeqDataSet stores expression data from RNA-seq and tissue type and replicates
+# makes it useable for differential expression analysis
 narrowdata = DESeqDataSetFromMatrix(countData=as.matrix(narrow), colData=narrow_metadata, design=~Tissue)
 
+# variance stabilizing transformation (VST) on narrowdata 
+# normalize and transform your raw count data for visualization and downstream analysis (like PCA, clustering, or heatmaps). <- from google
 narrowvstdata=vst(narrowdata)
-                  
+
+### step 3.3 ###     
+
 # Plot variance by average
 meanSdPlot(assay(narrowvstdata))
+
+# PCA (Principal Component Analysis) "is a dimensionality reduction technique used to simplify complex datasets by identifying patterns and highlighting the most important features or components in the data"
 
 narrowvstPCAdata = plotPCA(narrowvstdata,intgroup=c("Replicate","Tissue"), returnData=TRUE)
 ggplot(narrowvstPCAdata, aes(PC1, PC2, color=Tissue, shape=Replicate)) +
   geom_point(size=5) +
   labs(title = "PCA - Midgut Tissues")
-  ggsave("~/qbb2024-answers/week5/PCAplot.png")
+  
 
 
 
-### step 3.3 ###
-
-# Create PCA data
-narrowPcaData = plotPCA(narrowLogdata,intgroup=c("rep","tissue"), returnData=TRUE)
-
-# Plot PCA data
-ggplot(narrowPcaData, aes(PC1, PC2, color=tissue, shape=rep)) +
-  geom_point(size=5)
-
-# Batch-correct data to remove excess variance with variance stabilizing transformation
-narrowVstdata = vst(narrowdata)
-
-# Plot variance by average to verify the removal of batch-effects
-meanSdPlot(assay(narrowVstdata))
-
-# Perform PCA and plot to check batch-correction
-narrowPcaData = plotPCA(narrowVstdata,intgroup=c("rep","tissue"), returnData=TRUE)
-ggplot(narrowPcaData, aes(PC1, PC2, color=tissue, shape=rep)) +
-  geom_point(size=5)
+### step 3.4 ###
 
 # Convert into a matrix
-narrowVstdata = as.matrix(assay(narrowVstdata))
+narrowvstdata = as.matrix(assay(narrowvstdata))
 
 # Find replicate means
-combined = narrowVstdata[,seq(1, 21, 3)]
-combined = combined + narrowVstdata[,seq(2, 21, 3)]
-combined = combined + narrowVstdata[,seq(3, 21, 3)]
+combined = narrowvstdata[,seq(1, 21, 3)]
+combined = combined + narrowvstdata[,seq(2, 21, 3)]
+combined = combined + narrowvstdata[,seq(3, 21, 3)]
 combined = combined / 3
 
 # Use replicate means to filter low variance genes out
 filt = rowSds(combined) > 1
-narrowVstdata = narrowVstdata[filt,]
+narrowvstdata = narrowvstdata[filt,]
+
 
 # Plot expression values with hierarchical clustering
-heatmap(narrowVstdata, Colv=NA)
+# heatmap(narrowvstdata, Colv=NA)
 
 # Perform new hierarchical clustering with different clustering method
-distance = dist(narrowVstdata)
-Z = hclust(distance, method='ave')
+#distance = dist(narrowvstdata)
+#Z = hclust(distance, method='ave')
 
 # Plot expression values with new hierarchical clustering
-heatmap(narrowVstdata, Colv=NA, Rowv=as.dendrogram(Z))
+#heatmap(narrowvstdata, Colv=NA, Rowv=as.dendrogram(Z))
 
 # Set seed so this clustering is reproducible
 set.seed(42)
 
+##step 3.5##
+
 # Cluster genes using k-means clustering
-k=kmeans(narrowVstdata, centers=12)$cluster
+k=kmeans(narrowvstdata, centers=12)$cluster
 
 # Find ordering of samples to put them into their clusters
 ordering = order(k)
@@ -116,15 +112,15 @@ ordering = order(k)
 k = k[ordering]
 
 # Plot heatmap of expressions and clusters
-heatmap(narrowVstdata[ordering,],Rowv=NA,Colv=NA,RowSideColors = RColorBrewer::brewer.pal(12,"Paired")[k])
+heatmap(narrowvstdata[ordering,],Rowv=NA,Colv=NA,RowSideColors = RColorBrewer::brewer.pal(12,"Paired")[k])
 
 # Save heatmap
-png("heatmap.jpg")
-heatmap(narrowVstdata[ordering,],Rowv=NA,Colv=NA,RowSideColors = RColorBrewer::brewer.pal(12,"Paired")[k])
-dev.off()
+#png("heatmap.jpg")
+
+##step 3.6##
 
 # Pull out gene names from a specific cluster
-genes = rownames(narrowVstdata[k == 9,])
+genes = rownames(narrowvstdata[k == 9,])
 
 # Same gene names to a text file
 write.table(genes, "cluster_genes.txt", sep="\n", quote=FALSE, row.names=FALSE, col.names=FALSE)
