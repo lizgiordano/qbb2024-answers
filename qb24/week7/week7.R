78b1df2663b0b62428d5b4b46ead534b1f5d999d
Elizabeth Giordano <egiorda6@jh.edu>
2024-10-28 20:55:14 -0400

    week7 R

--- qb24/week7/week7.R
+++ qb24/week7/week7.R
@@ -9,50 +9,71 @@ library(DESeq2)
 # set your working directory to where your data and output will be stored
 # setwd("~/cmdb/qbb2024-answers/qb24/week7/")
 
+
+# STEP 1.1
+
+
 # load the gene expression counts
-counts_df <- read_delim("pasilla_gene_counts.tsv")
+counts_df <- read_delim("gtex_whole_blood_counts_downsample.txt")
 # move the gene_id column to rownames, so that the contents of the
 # tibble is entirely numeric
 
-counts_df <- column_to_rownames(counts_df, var = "gene_id")
+counts_df <- column_to_rownames(counts_df, var = "GENE_NAME")
 
 # look at first five rows
 
-counts_df[1:5,]
+# counts_df[1:5,]
 
 # load the metadata
-metadata_df <- read_delim("pasilla_metadata.csv")
-metadata_df[1:5,]
+metadata_df <- read_delim("gtex_metadata_downsample.txt")
+# metadata_df[1:5,]
 
-# move the sample IDs from the first column to rownames
-metadata_df <- column_to_rownames(metadata_df, var = "SAMPLE_ID")
+# move the sample IDs from the first column to row names
+metadata_df <- column_to_rownames(metadata_df, var = "SUBJECT_ID")
 
-
-
+# confirm that column and row names are correct 
 # check column names grabs column names
-# check that the columns of the counts are identical and in the same order as the
-# rows of the metadata
+# check that the columns of the counts are identical and in the same order as the rows of the metadata
 colnames(counts_df)
 rownames(metadata_df)
 table(colnames(counts_df) == rownames(metadata_df))
 
+# STEP 1.2
 
-# create a DESeq object
+# create a DESeq object using the counts and meta data using variables sex, age, and dthhrdy
 dds <- DESeqDataSetFromMatrix(countData = counts_df,
                               colData = metadata_df,
-                              design = ~ condition + type)
-#dds in console -> counts, row names, colnames
+                              design = ~ SEX + AGE + DTHHRDY)
 
 
+
+# STEP 1.3
 
 # apply VST normalization
+# give genes with different expression levels equal weights
+# store as variance stabilized data
 vsd <- vst(dds)
 
-# apply and plot principal components
-plotPCA(vsd, intgroup = "condition")
-plotPCA(vsd, intgroup = "type") 
-# type PC2 explains 30% of variance
-plotPCA(vsd, intgroup = c("condition", "type"))
+# plot to visualize the principal component analysis (PCA)
+# PCA allows you to group genes and create clusters 
+
+plotPCA(vsd, intgroup = "SEX") + labs(title= "PCA Analysis by Sex")
+
+plotPCA(vsd, intgroup = "AGE") + labs(title = "PCA Analysis by Age")
+
+plotPCA(vsd, intgroup = "DTHHRDY") + labs(title = "PCA Analysis by Cause of Death")
+
+
+##### 1.3.3 Question:
+# What proportion of variance in the gene expression data is explained by each of the first two principal components? 
+# 7% and 48%
+# Which principal components appear to be associated with which subject-level variables? 
+# Age clusters with PC1
+
+
+## Exercise 2: Perform differential expression analysis
+
+# STEP 2.1 test for differential expression between the sexes
 
 # get data in a format to allow to fit a linear model
 
@@ -61,6 +82,17 @@ vsd_df <- assay(vsd) %>%
   t() %>%
   as_tibble()
 vsd_df <- bind_cols(metadata_df, vsd_df)
+
+
+m1 <- lm(formula = WASH7P ~ DTHHRDY + AGE + SEX, data = vsd_df) %>%
+  summary() %>%
+  tidy()
+
+
+
+
+
+
 
 # examine the distribution of expression for a given gene
 hist(vsd_df$FBgn0000008)
