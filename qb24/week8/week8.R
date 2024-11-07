79a08a2b4878be9aba4c5260feae98f3f5f28350
Elizabeth Giordano <egiorda6@jh.edu>
2024-11-07 15:01:07 -0500

    week 8 R hw

--- qb24/week8/week8.R
+++ qb24/week8/week8.R
@@ -24,7 +24,6 @@ print(gut)
   # X-pca, X_tsne, and X_umap 
 
 # Question 2 Inspect the available cell metadata
-
 # How many columns are there in colData(gut)?
 colData(gut)
 colnames(colData(gut))
@@ -34,16 +33,28 @@ colnames(colData(gut))
   # sizeFactor can normalize the data so they are comparable between cell types. 
   # n_gene gives the number of genes for each cell so you can identify cells with high or low gene counts
 
-
 #Plot cells according to X_umap using plotReducedDim() and colouring by broad_annotation
+plotReducedDim(gut,"X_umap",color="broad_annotation")
 
 
 
+# Explore data
+# Sum the expression of each gene across all cells
+# Create a vector named genecounts by using rowSums() on the counts matrix returned by assay(gut)
 
+genecounts = rowSums(assay(gut)) # finds gene counts to see expression levels
+summary(genecounts)
+sort(genecounts, TRUE) # TRUE sets in descending order, use to find highest expressed genes
 
-
-
-
+#Question 3: Explore the genecounts distribution (1 pt)
+#What is the mean and median genecount according to summary()?
+  ## mean=3185 median=254
+# What might you conclude from these numbers?
+  ## most genes are on the low end of expression but a few have dramatically higher expression that skew the mean
+# What are the three genes with the highest expression after using sort()? 
+  ## Hsromega, CR45845, and roX1
+# What do they share in common?
+  ## all 3 are RNAs
 
 
 
